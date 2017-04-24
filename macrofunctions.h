7a42ad334ca281d714ba202a4257d96f83386b68
hgjun <hgjun@idb.snu.ac.kr>
2017-04-24 16:09:31 +0900

    replace indent area using spaces instead of tab

--- macrofunctions.h
+++ macrofunctions.h
@@ -12,35 +12,35 @@
 #define MAX(x, y)                                          ((x) >= (y) ? (x) : (y))
 
 #define SWAP(x, y)                                         do {\
-														       int tmp_ = (x);\
+                                                               int tmp_ = (x);\
                                                                (x) = (y);\
-															   (y) = tmp_;\
+                                                               (y) = tmp_;\
                                                            } while (0)
 #define FILL_ARRAY(arr, size, v)                           do {\
-														       int i_;\
-															   for (i_ = size; i_ >= 0; i_--)\
-															       arr[i_] = v;\
+                                                               int i_;\
+                                                               for (i_ = size; i_ >= 0; i_--)\
+                                                                   arr[i_] = v;\
                                                            } while (0)
 #define COPY_ARRAY(dest, d_begin, source, s_begin, s_end)  do {\
-														       int i_, j_ = (d_begin);\
-															   for (i_ = (s_begin); i_ <= (s_end); i_++)\
-															       dest[j_++] = source[i_];\
-														   } while (0)
+                                                               int i_, j_ = (d_begin);\
+                                                               for (i_ = (s_begin); i_ <= (s_end); i_++)\
+                                                                   dest[j_++] = source[i_];\
+                                                           } while (0)
 #define FILL_MATRIX(mat, row, col, v)                      do {\
-														       int i_, j_;\
-															   for (i_ = (row); i_ >= 0; i_--)\
-															       for (j_ = (col); j_ >= 0; j_--)\
-															           mat[i_][j_] = (v);\
-														   } while (0)
+                                                               int i_, j_;\
+                                                               for (i_ = (row); i_ >= 0; i_--)\
+                                                                   for (j_ = (col); j_ >= 0; j_--)\
+                                                                       mat[i_][j_] = (v);\
+                                                           } while (0)
 #define COPY_MATRIX(dest, source, row, col)                do {\
-														       int i_, j_;\
-															   for (i_ = (row); i_ >= 0; i_--)\
-															       for (j_ = (col); j_ >= 0; j_--)\
-															           dest[i_][j_] = source[i_][j_];\
-														   } while (0)
+                                                               int i_, j_;\
+                                                               for (i_ = (row); i_ >= 0; i_--)\
+                                                                   for (j_ = (col); j_ >= 0; j_--)\
+                                                                       dest[i_][j_] = source[i_][j_];\
+                                                           } while (0)
 #define TR_MATRIX(dest, source, row, col)                  do {\
 														       int i_, j_;\
 															   for (i_ = (row); i_ >= 0; i_--)\
-															       for (j_ = (col); j_ >= 0; j_--)\
-															           dest[j_][i_] = source[i_][j_];\
-														   } while (0)
+                                                                   for (j_ = (col); j_ >= 0; j_--)\
+                                                                       dest[j_][i_] = source[i_][j_];\
+                                                           } while (0)
